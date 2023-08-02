e8331e5e75cac1af4f48e3d1d50352fb5856508a
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-08-02 17:04:15 -0400

    Server now accepts parsed requests to create entries

--- Application/app.py
+++ Application/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from testing.OldClass.testing import Message, Stats, StatItem, Item, ShopItem
 from models.models import *
 from models.extensions import db
 from modelTesting import create_models
+from json import loads
 
 from testing.OldClass.testing import *
 
@@ -51,11 +52,57 @@ def get_shop(type_=""):
 
     return render_template(page, tier=tier, pcoins=pcoins, level=level, items=items)
 
+@app.route("/add/user/", methods=["POST"])
+def add_user():
+    data = loads(request.json)
+    fighter = Fighter(
+        id=data['id'],
+        name=data['name'],
+        level=data['level'],
+        health=data['health'],
+        power=data['power'],
+        heal_chance=data['heal'],
+        crit_chance=data['crit'],
+        weapon_id=data['weapon'],
+        armour_id=data['armour'],
+        ability_id=data['ability'],
+        passive_id=data['passive'],
+        tier=data['tier'],
+        paradians=data['paradians'],
+        inventory=data['inventory']
+    )
+    db.session.add(fighter)
+    db.session.commit()
+    return {"Status": "success"}
 
+@app.route("/add/enemy/", methods=["POST"])
+def add_enemy():
+    pass
+
+@app.route("/add/weapon/", methods=["POST"])
+def add_weapon():
+    pass
+
+@app.route("/add/armour/", methods=["POST"])
+def add_armour():
+    pass
+
+@app.route("/add/ability/", methods=["POST"])
+def add_ability():
+    pass
+
+def create_new_character(**kwargs):
+    character = Character(**kwargs) 
+    db.session.add(character)
+    db.session.commit()
+
+def create_new_item_base(**kwargs):
+    base_item = BaseItem(**kwargs)
+    db.session.add(base_item)
+    db.session.commit()
 
 with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    create_models()
     app.run(host='0.0.0.0', port=20000, debug=True)
--- Application/modelTesting.py
+++ Application/modelTesting.py
@@ -1,8 +1,9 @@
 """In this file, we will test creating models!"""
-from models.models import *
-from os import listdir
+from requests import post
+from json import dumps
 
 base = "./testing/Model/"
+URL = "http://127.0.0.1:20000/add/"
 
 def readlines(filename:str):
     with open(base+filename+".txt") as fp:
@@ -22,26 +23,43 @@ def get_info(filename):
             target: str = l[i]
             if "_" in target: target = target.replace("_", " ")
             if target.isnumeric(): target = int(target)
-            if target == -1: target = None
+            if target in [-1, "-1"]: target = None
 
             datum[fields[i]] = target 
         data.append(datum)
     return data
 
+def make_request(endpoint:str, items:list):
+    for item in items:
+        try:
+            resp = post(URL+endpoint+"/", json=dumps(item))
+            resp.raise_for_status()
+            print(resp.json())
+        except:
+            print("An error occurred for ", item)
+
 
 def create_models():
+    # Getting the info
+    # items = get_info("items")
     characters = get_info("characters")
     fighters = get_info("fighters")
     enemies = get_info('enemies')
     base_items = get_info("base")
-    # items = get_info("items")
     weapons = get_info("weapons")
     armours = get_info("armours")
     abilities = get_info("abilities")
     passives  = get_info("passives")
 
-    print(characters, fighters, base_items, abilities)
-
+    # Making the requests to update for fighter
+    c = [character for character in characters if character['id'] == fighters[0]['id']]
+    print(c)
+    c[0].update(fighters[0])
+    print(dumps(c[0]))
+    # print(data)
+    res = post(URL+"user/", json=dumps(c[0]))
+    res.raise_for_status()
+    print(res.json())
 
 if __name__ == "__main__":
     create_models()
