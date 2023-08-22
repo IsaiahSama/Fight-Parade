e73a5635f2efe95d81b0a59c1da92081224bafdc
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-08-22 16:43:28 -0400

    Added messages for quest and training

--- Application/static/scripts/game.js
+++ Application/static/scripts/game.js
@@ -21,7 +21,18 @@ const sendSystemMessage = (content) => {
   sendMessage("system", "system", content);
 };
 
+// Gaming Buttons
 const doJob = () => {
   sendPlayerMessage("I want to do a job!");
   sendSystemMessage("Oh you want to do a job I see. Bet!");
+};
+
+const doQuest = () => {
+  sendPlayerMessage("I'm making a reQUEST");
+  sendSystemMessage("Comedy... Alright, here's what I have for you.");
+};
+
+const doTraining = () => {
+  sendPlayerMessage("TIME TO TRAIN!");
+  sendSystemMessage("That's the spirit!");
 };
