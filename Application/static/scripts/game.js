3e8554b05ae63e0ab6b8e6fc6d9ca7722813f8f6
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-08-23 19:19:47 -0400

    Will now restore action buttons 5 seconds after

--- Application/static/scripts/game.js
+++ Application/static/scripts/game.js
@@ -44,7 +44,17 @@ const sendSystemMessage = (content) => {
   sendMessage("system", "system", content);
 };
 
-// Gaming Buttons
+// Action Buttons
+const restoreActions = () => {
+  const container = document.getElementById("actionButtonsContainer");
+  fetch("/get/buttons/action/")
+    .then((resp) => resp.text())
+    .then((data) => {
+      container.innerHTML = data;
+      htmx.process(container);
+    });
+};
+
 const doJob = () => {
   sendPlayerMessage("I want to do a job!");
   sendSystemMessage("Let's see what we have available.");
@@ -59,11 +69,14 @@ const doJob = () => {
       sendSystemMessage("<b>You " + data["STATUS"] + "</b>");
       sendSystemMessage("<i>" + data["RESPONSE"] + "</i>");
     });
+
+  setTimeout(restoreActions, 5000);
 };
 
 const doQuest = () => {
   sendPlayerMessage("I'm making a reQUEST");
   sendSystemMessage("Comedy... Alright, here's what I have for you.");
+  setTimeout(restoreActions, 5000);
 };
 
 const doTraining = () => {
